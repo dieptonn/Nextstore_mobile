9bd5f9f6797217c9925c2ca7ca103bc8398191d5
dieptonn <diep.bt194505@sis.hust.edu.vn>
2024-05-11 00:46:29 +0700

    update review screen

--- src/screens/reviews.js
+++ src/screens/reviews.js
@@ -3,6 +3,15 @@ import { View, Text, Image, ScrollView, TextInput, StyleSheet, TouchableOpacity 
 
 import back2 from '../assets/images/back2.png';
 import search from '../assets/images/search.png';
+import star from '../assets/images/star.png';
+import notstar from '../assets/images/notstar.png';
+import ava1 from '../assets/images/ava1.png';
+import cmtImg1 from '../assets/images/cmtImg1.png';
+import cmtImg2 from '../assets/images/cmtImg2.png';
+import cmtImg3 from '../assets/images/cmtImg3.png';
+
+
+
 import { useNavigation } from '@react-navigation/native';
 
 
@@ -36,6 +45,213 @@ const ReviewScreen = () => {
                     placeholderTextColor="#A9A9A9"
                 />
             </View>
+            <ScrollView style={styles.cmt}>
+                <View style={styles.cmtDiv}>
+                    <View style={styles.rating}>
+                        <View style={styles.star}>
+                            <Image source={star} style={styles.ratingImg}></Image>
+                            <Image source={star} style={styles.ratingImg}></Image>
+                            <Image source={star} style={styles.ratingImg}></Image>
+                            <Image source={star} style={styles.ratingImg}></Image>
+                            <Image source={notstar} style={styles.ratingImg}></Image>
+                        </View>
+                        <Text style={styles.time}>
+                            Today, 12:30 pm
+                        </Text>
+                    </View>
+                    <View style={styles.user}>
+                        <Image source={ava1} style={styles.ava}></Image>
+                        <Text style={styles.tit}>Erin Mango</Text>
+                    </View>
+                    <Text style={styles.time}>Everything is good. Nice quality</Text>
+                    <View style={styles.rv}>
+                        <Image source={cmtImg1} style={styles.rvImg}></Image>
+                        <Image source={cmtImg2} style={styles.rvImg}></Image>
+                        <Image source={cmtImg3} style={styles.rvImg}></Image>
+                    </View>
+                </View>
+
+                <View style={styles.cmtDiv}>
+                    <View style={styles.rating}>
+                        <View style={styles.star}>
+                            <Image source={star} style={styles.ratingImg}></Image>
+                            <Image source={star} style={styles.ratingImg}></Image>
+                            <Image source={star} style={styles.ratingImg}></Image>
+                            <Image source={notstar} style={styles.ratingImg}></Image>
+                            <Image source={notstar} style={styles.ratingImg}></Image>
+                        </View>
+                        <Text style={styles.time}>
+                            1 month ago
+                        </Text>
+                    </View>
+                    <View style={styles.user}>
+                        <Image source={ava1} style={styles.ava}></Image>
+                        <Text style={styles.tit}>Corey Rosser</Text>
+                    </View>
+                    <Text style={styles.time}>Could be better :(</Text>
+
+                </View>
+
+                <View style={styles.cmtDiv}>
+                    <View style={styles.rating}>
+                        <View style={styles.star}>
+                            <Image source={star} style={styles.ratingImg}></Image>
+                            <Image source={star} style={styles.ratingImg}></Image>
+                            <Image source={star} style={styles.ratingImg}></Image>
+                            <Image source={star} style={styles.ratingImg}></Image>
+                            <Image source={star} style={styles.ratingImg}></Image>
+                        </View>
+                        <Text style={styles.time}>
+                            2 months ago
+                        </Text>
+                    </View>
+                    <View style={styles.user}>
+                        <Image source={ava1} style={styles.ava}></Image>
+                        <Text style={styles.tit}>Paityn Saris</Text>
+                    </View>
+                    <Text style={styles.time}>Bought this table 2 months ago and I{'\n'}wanted to say, this is the best bedside {'\n'}table I’ve ever used 😍</Text>
+
+                </View>
+
+
+                <View style={styles.cmtDiv}>
+                    <View style={styles.rating}>
+                        <View style={styles.star}>
+                            <Image source={star} style={styles.ratingImg}></Image>
+                            <Image source={star} style={styles.ratingImg}></Image>
+                            <Image source={star} style={styles.ratingImg}></Image>
+                            <Image source={star} style={styles.ratingImg}></Image>
+                            <Image source={notstar} style={styles.ratingImg}></Image>
+                        </View>
+                        <Text style={styles.time}>
+                            Today, 12:30 pm
+                        </Text>
+                    </View>
+                    <View style={styles.user}>
+                        <Image source={ava1} style={styles.ava}></Image>
+                        <Text style={styles.tit}>Erin Mango</Text>
+                    </View>
+                    <Text style={styles.time}>Everything is good. Nice quality</Text>
+                    <View style={styles.rv}>
+                        <Image source={cmtImg1} style={styles.rvImg}></Image>
+                        <Image source={cmtImg2} style={styles.rvImg}></Image>
+                        <Image source={cmtImg3} style={styles.rvImg}></Image>
+                    </View>
+                </View>
+
+                <View style={styles.cmtDiv}>
+                    <View style={styles.rating}>
+                        <View style={styles.star}>
+                            <Image source={star} style={styles.ratingImg}></Image>
+                            <Image source={star} style={styles.ratingImg}></Image>
+                            <Image source={star} style={styles.ratingImg}></Image>
+                            <Image source={notstar} style={styles.ratingImg}></Image>
+                            <Image source={notstar} style={styles.ratingImg}></Image>
+                        </View>
+                        <Text style={styles.time}>
+                            1 month ago
+                        </Text>
+                    </View>
+                    <View style={styles.user}>
+                        <Image source={ava1} style={styles.ava}></Image>
+                        <Text style={styles.tit}>Corey Rosser</Text>
+                    </View>
+                    <Text style={styles.time}>Could be better :(</Text>
+
+                </View>
+
+                <View style={styles.cmtDiv}>
+                    <View style={styles.rating}>
+                        <View style={styles.star}>
+                            <Image source={star} style={styles.ratingImg}></Image>
+                            <Image source={star} style={styles.ratingImg}></Image>
+                            <Image source={star} style={styles.ratingImg}></Image>
+                            <Image source={star} style={styles.ratingImg}></Image>
+                            <Image source={star} style={styles.ratingImg}></Image>
+                        </View>
+                        <Text style={styles.time}>
+                            2 months ago
+                        </Text>
+                    </View>
+                    <View style={styles.user}>
+                        <Image source={ava1} style={styles.ava}></Image>
+                        <Text style={styles.tit}>Paityn Saris</Text>
+                    </View>
+                    <Text style={styles.time}>Bought this table 2 months ago and I{'\n'}wanted to say, this is the best bedside {'\n'}table I’ve ever used 😍</Text>
+
+                </View>
+
+                <View style={styles.cmtDiv}>
+                    <View style={styles.rating}>
+                        <View style={styles.star}>
+                            <Image source={star} style={styles.ratingImg}></Image>
+                            <Image source={star} style={styles.ratingImg}></Image>
+                            <Image source={star} style={styles.ratingImg}></Image>
+                            <Image source={star} style={styles.ratingImg}></Image>
+                            <Image source={notstar} style={styles.ratingImg}></Image>
+                        </View>
+                        <Text style={styles.time}>
+                            Today, 12:30 pm
+                        </Text>
+                    </View>
+                    <View style={styles.user}>
+                        <Image source={ava1} style={styles.ava}></Image>
+                        <Text style={styles.tit}>Erin Mango</Text>
+                    </View>
+                    <Text style={styles.time}>Everything is good. Nice quality</Text>
+                    <View style={styles.rv}>
+                        <Image source={cmtImg1} style={styles.rvImg}></Image>
+                        <Image source={cmtImg2} style={styles.rvImg}></Image>
+                        <Image source={cmtImg3} style={styles.rvImg}></Image>
+                    </View>
+                </View>
+
+                <View style={styles.cmtDiv}>
+                    <View style={styles.rating}>
+                        <View style={styles.star}>
+                            <Image source={star} style={styles.ratingImg}></Image>
+                            <Image source={star} style={styles.ratingImg}></Image>
+                            <Image source={star} style={styles.ratingImg}></Image>
+                            <Image source={notstar} style={styles.ratingImg}></Image>
+                            <Image source={notstar} style={styles.ratingImg}></Image>
+                        </View>
+                        <Text style={styles.time}>
+                            1 month ago
+                        </Text>
+                    </View>
+                    <View style={styles.user}>
+                        <Image source={ava1} style={styles.ava}></Image>
+                        <Text style={styles.tit}>Corey Rosser</Text>
+                    </View>
+                    <Text style={styles.time}>Could be better :(</Text>
+
+                </View>
+
+                <View style={styles.cmtDiv}>
+                    <View style={styles.rating}>
+                        <View style={styles.star}>
+                            <Image source={star} style={styles.ratingImg}></Image>
+                            <Image source={star} style={styles.ratingImg}></Image>
+                            <Image source={star} style={styles.ratingImg}></Image>
+                            <Image source={star} style={styles.ratingImg}></Image>
+                            <Image source={star} style={styles.ratingImg}></Image>
+                        </View>
+                        <Text style={styles.time}>
+                            2 months ago
+                        </Text>
+                    </View>
+                    <View style={styles.user}>
+                        <Image source={ava1} style={styles.ava}></Image>
+                        <Text style={styles.tit}>Paityn Saris</Text>
+                    </View>
+                    <Text style={styles.time}>Bought this table 2 months ago and I{'\n'}wanted to say, this is the best bedside {'\n'}table I’ve ever used 😍</Text>
+
+                </View>
+
+                <View style={styles.space}>
+
+                </View>
+            </ScrollView>
         </View>
     );
 };
@@ -98,6 +314,59 @@ const styles = StyleSheet.create({
         fontSize: 16,
         color: '#333333',
     },
+    cmt: {
+        // position: 'absolute',
+        top: 210,
+        flexDirection: 'column',
+        width: '100%',
+        // padding: 16
+
+    },
+    cmtDiv: {
+        padding: 16,
+        width: '100%',
+        flexDirection: 'column',
+        gap: 12
+    },
+    rating: {
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    star: {
+        flexDirection: 'row',
+        gap: 3
+    },
+    ratingImg: {
+        width: 16,
+        height: 16,
+    },
+    time: {
+        fontSize: 14,
+        // fontWeight: 'bold',
+        fontFamily: 'notoserif',
+        color: '#9E9E9E',
+        lineHeight: 24
+    },
+    user: {
+        flexDirection: 'row',
+        gap: 8
+    },
+    ava: {
+        width: 24,
+        height: 24
+    },
+    rv: {
+        flexDirection: 'row',
+        gap: 8
+    },
+    rvImg: {
+        width: 54,
+        height: 54
+    },
+    space: {
+        height: 250
+    }
 });
 
 export default ReviewScreen;
